2b68ed4ee3bac491de0a799d63a0141f33491cda
Ryan Stephens <diving.bc@gmail.com>
2017-03-18 20:44:35 -0700

    updated sidebar menu to contain base CRUD links

--- resources/views/admin/sidebar/navigation.blade.php
+++ resources/views/admin/sidebar/navigation.blade.php
@@ -25,18 +25,196 @@
                         </li>
 
                         <!--Menu list item-->
-                         <li>
-                            <a href="#">
-                                <i class="fa fa-laptop"></i>
+                        <!-- Authors -->
+                        <li>
+                            <a href="{{ route('admin.authors.index') }}">
+                                <i class="fa fa-user"></i>
                                 <span class="menu-title">
-                                    <strong>Main</strong>
+                                    <strong>Authors</strong>
                                 </span>
                                 <i class="arrow"></i>
                             </a>
                             <!--Submenu-->
                             <ul class="collapse in">
-                                <li><a href="#">Sub</a></li>
-                                <li class="list-divider"></li>
+                                <li>
+                                    <a href="{{ route('admin.authors.index') }}">
+                                        <i class="fa fa-eye"></i>
+                                        <span class="menu-title">
+                                            <strong>View all</strong>
+                                        </span>
+                                        <i class="arrow"></i>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="{{ route('admin.authors.create') }}">
+                                        <i class="fa fa-pencil"></i>
+                                        <span class="menu-title">
+                                            <strong>Create</strong>
+                                        </span>
+                                        <i class="arrow"></i>
+                                    </a>
+                                </li>
+                            </ul>
+                        </li>
+
+
+                        <!-- Locations -->
+                        <li>
+                            <a href="{{ route('admin.locations.index') }}">
+                                <i class="fa fa-map-marker"></i>
+                                <span class="menu-title">
+                                    <strong>Locations</strong>
+                                </span>
+                                <i class="arrow"></i>
+                            </a>
+                            <!--Submenu-->
+                            <ul class="collapse in">
+                                <li>
+                                    <a href="{{ route('admin.locations.index') }}">
+                                        <i class="fa fa-eye"></i>
+                                        <span class="menu-title">
+                                            <strong>View all</strong>
+                                        </span>
+                                        <i class="arrow"></i>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="{{ route('admin.locations.create') }}">
+                                        <i class="fa fa-pencil"></i>
+                                        <span class="menu-title">
+                                            <strong>Create</strong>
+                                        </span>
+                                        <i class="arrow"></i>
+                                    </a>
+                                </li>
+                            </ul>
+                        </li>
+
+                        <!-- Trips -->
+                        <li>
+                            <a href="{{ route('admin.trips.index') }}">
+                                <i class="fa fa-plane"></i>
+                                <span class="menu-title">
+                                    <strong>Trips</strong>
+                                </span>
+                                <i class="arrow"></i>
+                            </a>
+                            <!--Submenu-->
+                            <ul class="collapse in">
+                                <li>
+                                    <a href="{{ route('admin.trips.index') }}">
+                                        <i class="fa fa-eye"></i>
+                                        <span class="menu-title">
+                                            <strong>View all</strong>
+                                        </span>
+                                        <i class="arrow"></i>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="{{ route('admin.trips.create') }}">
+                                        <i class="fa fa-pencil"></i>
+                                        <span class="menu-title">
+                                            <strong>Create</strong>
+                                        </span>
+                                        <i class="arrow"></i>
+                                    </a>
+                                </li>
+                            </ul>
+                        </li>
+
+                        <!-- Blogs -->
+                        <li>
+                            <a href="{{ route('admin.blogs.index') }}">
+                                <i class="fa fa-th-large"></i>
+                                <span class="menu-title">
+                                    <strong>Blogs</strong>
+                                </span>
+                                <i class="arrow"></i>
+                            </a>
+                            <!--Submenu-->
+                            <ul class="collapse in">
+                                <li>
+                                    <a href="{{ route('admin.blogs.index') }}">
+                                        <i class="fa fa-eye"></i>
+                                        <span class="menu-title">
+                                            <strong>View all</strong>
+                                        </span>
+                                        <i class="arrow"></i>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="{{ route('admin.blogs.create') }}">
+                                        <i class="fa fa-pencil"></i>
+                                        <span class="menu-title">
+                                            <strong>Create</strong>
+                                        </span>
+                                        <i class="arrow"></i>
+                                    </a>
+                                </li>
+                            </ul>
+                        </li>
+
+                        <!-- Maps -->
+                        <li>
+                            <a href="{{ route('admin.maps.index') }}">
+                                <i class="fa fa-map"></i>
+                                <span class="menu-title">
+                                    <strong>Maps</strong>
+                                </span>
+                                <i class="arrow"></i>
+                            </a>
+                            <!--Submenu-->
+                            <ul class="collapse in">
+                                <li>
+                                    <a href="{{ route('admin.maps.index') }}">
+                                        <i class="fa fa-eye"></i>
+                                        <span class="menu-title">
+                                            <strong>View all</strong>
+                                        </span>
+                                        <i class="arrow"></i>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="{{ route('admin.maps.create') }}">
+                                        <i class="fa fa-pencil"></i>
+                                        <span class="menu-title">
+                                            <strong>Create</strong>
+                                        </span>
+                                        <i class="arrow"></i>
+                                    </a>
+                                </li>
+                            </ul>
+                        </li>
+
+                        <!-- Images -->
+                        <li>
+                            <a href="{{ route('admin.images.index') }}">
+                                <i class="fa fa-picture-o"></i>
+                                <span class="menu-title">
+                                    <strong>Images</strong>
+                                </span>
+                                <i class="arrow"></i>
+                            </a>
+                            <!--Submenu-->
+                            <ul class="collapse in">
+                                <li>
+                                    <a href="{{ route('admin.images.index') }}">
+                                        <i class="fa fa-eye"></i>
+                                        <span class="menu-title">
+                                            <strong>View all</strong>
+                                        </span>
+                                        <i class="arrow"></i>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="{{ route('admin.images.create') }}">
+                                        <i class="fa fa-pencil"></i>
+                                        <span class="menu-title">
+                                            <strong>Create</strong>
+                                        </span>
+                                        <i class="arrow"></i>
+                                    </a>
+                                </li>
                             </ul>
                         </li>
                     </ul>
